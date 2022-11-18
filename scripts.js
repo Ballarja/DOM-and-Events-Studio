dc3e763adcdbe3ce9c130453989f1025113a1cbb
James Ballard <jamesballard87@hotmail.com>
2022-11-17 20:45:26 -0600

    added events to script.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,22 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+
+function thisFucntion(){
+    const takeOff = document.getElementById("takeoff");
+    const flightStatus = document.getElementById("flightStatus");
+    const shuttleBackground = document.getElementById("shuttleBackground");
+
+    takeOff.addEventListener("click", event =>{
+        window.confirm("Confirm that the shuttle is ready for takeoff.")
+    });
+    takeOff.addEventListener("click", event =>{
+        flightStatus.innerHTML = "Shuttle in Flight?";
+    })
+    takeOff.addEventListener("click", function(event){
+        event.target.styles.shuttleBackground = "blue";
+    })
+    
+}
+
+window.addEventListener("load", thisFucntion);
